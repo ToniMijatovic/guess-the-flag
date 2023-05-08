888c5b86578fe3e25d747ae1acc20bea343c280e
toni <toni@dharma.nl>
2023-05-08 13:52:27 +0200

    Added Guess The Flag app

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Toni Mijatovic on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
